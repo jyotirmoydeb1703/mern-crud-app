74d1840260cc59fe764121eabaa7058c80d842b6
Jyotirmoy Deb <jyotirmoytrain@gmail.com>
2022-03-20 04:11:45 +0530

    Backend for crud

---
+++ controllers/getController.js
@@ -0,0 +1,45 @@
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../model/goalModel');
+
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+  res.status(200).json(goals);
+})
+
+const getGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get Goal' });
+})
+
+const setGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+  res.status(200).json(goal);
+})
+
+const updateGoal = asyncHandler(async (req, res) => {
+
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found');
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+  res.status(200).json(updatedGoal)
+});
+
+const deleteGoal = asyncHandler(async(req, res) => {
+  const goal = await Goal.findById(req.params.id)
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found');
+  }
+
+  await goal.remove()
+  res.status(200).json({id:req.params.id});
+})
+
+module.exports = { getGoals, getGoal, setGoal, updateGoal, deleteGoal }
